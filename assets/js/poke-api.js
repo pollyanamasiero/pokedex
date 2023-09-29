64e570143af1471ea438b80193d9a2f34f38c6f7
Pollyana Masiero Torquato <pollyanamasiero@hotmail.com>
2023-09-29 13:00:24 -0300

    projeto finalizado

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -15,6 +15,8 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
+    
+
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
@@ -23,8 +25,9 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 6) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}}&limit=${limit}`
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+   
 
     return fetch(url)
         .then((response) => response.json())
@@ -32,4 +35,5 @@ pokeApi.getPokemons = (offset = 0, limit = 6) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+     
 }
